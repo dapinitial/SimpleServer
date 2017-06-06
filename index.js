d74bb940205f8588cff2a9416a1d283becb40d0e
David Puerto <me@davidpuerto.com>
2017-06-05 21:55:06 -0700

    scaffolding, initial setup

---
+++ index.js
@@ -0,0 +1,15 @@
+// Initial Dependencies
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const app = express(); // App instance
+
+// App Setup
+
+
+// Server Setup
+const port = process.env.PORT || 3090
+const server = http.createServer(app);
+server.listen(port);
+console.log('Server listening on: ', port);
