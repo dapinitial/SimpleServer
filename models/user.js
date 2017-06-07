3420d50c652c94013fd1de834c00ceecabeae568
David Puerto <me@davidpuerto.com>
2017-06-06 23:02:24 -0700

    salting hash with bcrypt

--- models/user.js
+++ models/user.js
@@ -1,10 +1,28 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
 
 // Define our model
 const userSchema = new Schema({
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, lowercase: true },
   password: String
+});
+
+// On save Hook, encrypt password
+// Before saving a model, run this function:
+userSchema.pre('save', function(next) { // `pre` means before 'save' invoked, run function
+  const user = this; // get access to user mode. Not required. user.email, user.password though
+  // Generate a salt (takes time, so pass callback function)
+  bcrypt.genSalt(10, function(err, salt) {
+    if (err) { return next(err); }
+    // Hash (encrypt) password using the salt
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) { return next(err); }
+      // overwrite plain text password with encrypted password.
+      user.password = hash;
+      next(); // finish pre callback hell
+    });
+  });
 });
 
 // Create the Model Class
