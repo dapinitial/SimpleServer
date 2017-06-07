870543778a8135a12eaa9a5ee375a6d82ab7aa69
David Puerto <me@davidpuerto.com>
2017-06-06 23:47:47 -0700

    updating packages and prepping for JWT. adding config.js to .gitignore

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcrypt-nodejs');
+const jwt = require('jwt-simple');
 
 // Define our model
 const userSchema = new Schema({
