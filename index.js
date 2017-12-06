df0c5a9bdc0d38465d3bee50d0a4d65e8ca84b73
David Puerto <me@davidpuerto.com>
2017-12-06 03:42:32 -0800

    updating for CORS

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // Initial Dependencies
-const express = require('express');
-const http = require('http');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
+const express = require("express");
+const http = require("http");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
 const app = express(); // App instance
-const router = require('./router');
-const mongoose = require('mongoose');
+const router = require("./router");
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 // DB Setup
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:auth/auth');
+mongoose.connect("mongodb://localhost:auth/auth");
 
 // App Setup
-app.use(morgan('combined'));
-app.use(bodyParser.json({ type: '*/*'} ));
+app.use(morgan("combined"));
+app.use(cors());
+app.use(bodyParser.json({ type: "*/*" }));
 router(app);
 
 // Server Setup
 const port = process.env.PORT || 3090;
 const server = http.createServer(app);
 server.listen(port);
-console.log('Server listening on: ', port);
+console.log("Server listening on: ", port);
